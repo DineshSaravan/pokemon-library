9e5d088054153284d5371d089adfc60feafb9c52
saravand <dinesh.saravan@thehutgroup.com>
2021-09-28 02:37:52 +0100

    Pokemon api to connect local graphql

---
+++ src/api.tsx
@@ -0,0 +1,9 @@
+const pokeApi = {
+    baseUrl: "http://localhost:5000",
+    username: "dinesh.saravan",
+    headers: {
+        "Content-Type": "application/json",
+    }
+}
+
+export default pokeApi;
